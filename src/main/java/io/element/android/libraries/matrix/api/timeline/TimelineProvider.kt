5cf1dd15be6d3d54aff9306cc4b2487280257bc1
Benoit Marty <benoit@matrix.org>
2024-04-26 14:47:01 +0200

    Move LiveTimelineProvider to the test module.

--- src/main/java/io/element/android/libraries/matrix/api/timeline/TimelineProvider.kt
+++ src/main/java/io/element/android/libraries/matrix/api/timeline/TimelineProvider.kt
@@ -16,13 +16,8 @@
 
 package io.element.android.libraries.matrix.api.timeline
 
-import com.squareup.anvil.annotations.ContributesBinding
-import io.element.android.libraries.di.RoomScope
-import io.element.android.libraries.matrix.api.room.MatrixRoom
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.first
-import javax.inject.Inject
 
 /**
  * This interface defines a way to get the active timeline.
@@ -33,13 +28,3 @@ interface TimelineProvider {
 }
 
 suspend fun TimelineProvider.getActiveTimeline(): Timeline = activeTimelineFlow().first()
-
-/**
- * Default implementation of [TimelineProvider] that provides the live timeline of a room.
- */
-@ContributesBinding(RoomScope::class)
-class LiveTimelineProvider @Inject constructor(
-    private val room: MatrixRoom,
-) : TimelineProvider {
-    override fun activeTimelineFlow(): StateFlow<Timeline> = MutableStateFlow(room.liveTimeline)
-}
