721dadcb3284e2569f6a3628466f8242b61b89c6
SpiritCroc <dev@spiritcroc.de>
2025-11-24 21:38:30 +0100

    [TMP] Drop matrix module

--- matrix/src/main/java/chat/schildi/revenge/util/ScAppDirs.kt
+++
@@ -1,7 +0,0 @@
-package chat.schildi.revenge.util
-
-import ca.gosyer.appdirs.AppDirs
-
-val ScAppDirs = AppDirs {
-    appName = "SchildiChatRevenge"
-}
--- matrix/src/main/java/chat/schildi/revenge/util/Utils.kt
+++
@@ -1,9 +0,0 @@
-package chat.schildi.revenge.util
-
-inline fun <T>tryOrNull(block: () -> T): T? = try {
-    block()
-} catch (_: Throwable) {
-    null
-}
-
-fun String.escapeForFilename() = replace(Regex("[/\\\\:*?\"<>|]"), "_")
