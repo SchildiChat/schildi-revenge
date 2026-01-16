5416512a070c97d51c79041b4436fe00c05ad48f
SpiritCroc <dev@spiritcroc.de>
2026-01-16 21:18:04 +0100

    Fix tooltip crash
    Change-Id: Ia8e94bd4a44bf38a6f49ebae649ea856e5ef87c0

--- composeApp/src/jvmMain/kotlin/chat/schildi/revenge/compose/components/Tooltip.kt
+++ composeApp/src/jvmMain/kotlin/chat/schildi/revenge/compose/components/Tooltip.kt
@@ -1,14 +1,19 @@
 package chat.schildi.revenge.compose.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
@@ -20,8 +25,18 @@ fun WithTooltip(
 ) {
     TooltipBox(
         tooltip = {
-            PlainTooltip {
-                Text(text())
+            Box(Modifier
+                .background(
+                    MaterialTheme.colorScheme.surfaceContainerHighest,
+                    CircleShape,
+                )
+                .padding(horizontal = 8.dp, vertical = 4.dp)
+            ) {
+                Text(
+                    text(),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.labelSmall,
+                )
             }
         },
         state = rememberTooltipState(isPersistent = isPersistent),
