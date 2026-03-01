450768fb4a4e71031f14a9e40af0dd1aeafd80aa
SpiritCroc <dev@spiritcroc.de>
2026-03-01 11:55:03 +0100

    Propagate window title of current active split destination
    Change-Id: I9a46f29849933b264c4cd5caec18f76cedc3cca9

--- composeApp/src/jvmMain/kotlin/chat/schildi/revenge/compose/destination/split/SplitScreen.kt
+++ composeApp/src/jvmMain/kotlin/chat/schildi/revenge/compose/destination/split/SplitScreen.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import chat.schildi.revenge.compose.DestinationContent
 import chat.schildi.revenge.Destination
+import chat.schildi.revenge.DestinationStateHolder
 import chat.schildi.revenge.Dimens
+import chat.schildi.revenge.LocalDestinationState
 import chat.schildi.revenge.actions.FocusRole
 import chat.schildi.revenge.actions.LocalKeyboardActionHandler
 import chat.schildi.revenge.actions.LocalKeyboardActionProvider
@@ -19,6 +22,8 @@ import chat.schildi.revenge.compose.components.AdaptiveColumn
 import chat.schildi.revenge.compose.components.AdaptiveRow
 import chat.schildi.revenge.compose.focus.FlatFocusContainer
 import chat.schildi.revenge.compose.focus.rememberFocusId
+import chat.schildi.revenge.config.keybindings.DestinationEnum
+import co.touchlab.kermit.Logger
 import kotlinx.collections.immutable.persistentListOf
 import java.util.UUID
 
@@ -40,22 +45,18 @@ fun SplitHorizontal(
     ) {
         val primaryFocusId = rememberFocusId()
         val secondaryFocusId = rememberFocusId()
-        FlatFocusContainer(
-            LocalKeyboardActionProvider provides splitKeyboardActionProvider(true),
-            role = FocusRole.NESTING_DESTINATION_ROOT_CONTAINER,
+        SplitScreenDestination(
             focusId = primaryFocusId,
-            modifier = Modifier.splitContainerDecoration(primaryFocusId, SplitRole.Start),
-        ) { modifier ->
-            DestinationContent(destination.primary, modifier)
-        }
-        FlatFocusContainer(
-            LocalKeyboardActionProvider provides splitKeyboardActionProvider(false),
-            role = FocusRole.NESTING_DESTINATION_ROOT_CONTAINER,
+            splitRole = SplitRole.Start,
+            splitType = destination.type,
+            destinationHolder = destination.primary,
+        )
+        SplitScreenDestination(
             focusId = secondaryFocusId,
-            modifier = Modifier.splitContainerDecoration(secondaryFocusId, SplitRole.End),
-        ) { modifier ->
-            DestinationContent(destination.secondary, modifier)
-        }
+            splitRole = SplitRole.End,
+            splitType = destination.type,
+            destinationHolder = destination.secondary,
+        )
     }
 }
 
@@ -70,34 +71,68 @@ fun SplitVertical(
     ) {
         val primaryFocusId = rememberFocusId()
         val secondaryFocusId = rememberFocusId()
-        FlatFocusContainer(
-            LocalKeyboardActionProvider provides splitKeyboardActionProvider(true),
-            role = FocusRole.NESTING_DESTINATION_ROOT_CONTAINER,
+        SplitScreenDestination(
             focusId = primaryFocusId,
-            modifier = Modifier.splitContainerDecoration(primaryFocusId, SplitRole.Top),
-        ) { modifier ->
-            DestinationContent(destination.primary, modifier)
-        }
-        FlatFocusContainer(
-            LocalKeyboardActionProvider provides splitKeyboardActionProvider(false),
-            role = FocusRole.NESTING_DESTINATION_ROOT_CONTAINER,
+            splitRole = SplitRole.Top,
+            splitType = destination.type,
+            destinationHolder = destination.primary,
+        )
+        SplitScreenDestination(
             focusId = secondaryFocusId,
-            modifier = Modifier.splitContainerDecoration(secondaryFocusId, SplitRole.Bottom),
-        ) { modifier ->
-            DestinationContent(destination.secondary, modifier)
-        }
+            splitRole = SplitRole.Bottom,
+            splitType = destination.type,
+            destinationHolder = destination.secondary,
+        )
     }
 }
 
+@Composable
+fun SplitScreenDestination(
+    focusId: UUID,
+    splitRole: SplitRole,
+    splitType: DestinationEnum,
+    destinationHolder: DestinationStateHolder,
+    modifier: Modifier = Modifier,
+) {
+    val keyHandler = LocalKeyboardActionHandler.current
+    val focusedContainers = keyHandler.currentFocusedNestingDestinations.collectAsState(persistentListOf())
+    val isActive = focusedContainers.value.firstOrNull() == focusId
+    val hasActive = focusedContainers.value.contains(focusId)
+    val parentDestinationStateHolder = LocalDestinationState.current
+
+    // Publish current focused title for window title
+    if (parentDestinationStateHolder != null) {
+        val childTitle = destinationHolder.state.collectAsState().value.titleOverride
+        LaunchedEffect(hasActive, childTitle) {
+            if (hasActive) {
+                parentDestinationStateHolder.publishTitle(
+                    childTitle
+                ) {
+                    it.type == splitType
+                }
+            }
+        }
+    }
+
+    // Actual content
+    FlatFocusContainer(
+        LocalKeyboardActionProvider provides splitKeyboardActionProvider(false),
+        role = FocusRole.NESTING_DESTINATION_ROOT_CONTAINER,
+        focusId = focusId,
+        modifier = modifier.splitContainerDecoration(
+            isActive,
+            splitRole,
+        ),
+    ) { modifier ->
+        DestinationContent(destinationHolder, modifier)
+    }
+}
 
 @Composable
 private fun Modifier.splitContainerDecoration(
-    id: UUID,
+    isActive: Boolean,
     splitRole: SplitRole,
 ): Modifier {
-    val keyHandler = LocalKeyboardActionHandler.current
-    val focusedContainers = keyHandler.currentFocusedNestingDestinations.collectAsState(persistentListOf())
-    val isActive = focusedContainers.value.firstOrNull() == id
     val padding = Dimens.Split.outlineWidth
     val halfPadding = padding / 2
     val paddingValues = when (splitRole) {
