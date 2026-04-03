eb42a0461ef1b2c66a354b099e696edc655a3a8e
SpiritCroc <dev@spiritcroc.de>
2026-04-03 10:48:33 +0200

    Don't notify for past notifications on startup
    Change-Id: I3a6d8b5466e33922b04b13e0790a23e83a94dc2a

--- composeApp/src/jvmMain/kotlin/chat/schildi/revenge/notification/NotificationProcessor.kt
+++ composeApp/src/jvmMain/kotlin/chat/schildi/revenge/notification/NotificationProcessor.kt
@@ -5,6 +5,7 @@ import chat.schildi.preferences.RevengePrefs
 import chat.schildi.preferences.ScPrefs
 import chat.schildi.revenge.UiState
 import chat.schildi.revenge.plaintext.NotificationEventTextFormat
+import chat.schildi.revenge.util.tryOrNull
 import co.touchlab.kermit.Logger
 import io.element.android.libraries.matrix.api.media.MediaSource
 import kotlinx.coroutines.CoroutineScope
@@ -17,10 +18,15 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.shareIn
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.longOrNull
 
 object NotificationProcessor {
     private val log = Logger.withTag("NotificationProcessor")
     private val scope = CoroutineScope(Dispatchers.IO)
+    private val launchTimestamp = System.currentTimeMillis()
 
     @OptIn(ExperimentalCoroutinesApi::class)
     val notifications = combine(
@@ -47,12 +53,28 @@ object NotificationProcessor {
 
     fun observeNotifications() {
         notifications.onEach { notification ->
-            val formatted = NotificationEventTextFormat.notificationToText(notification)
             val id = notification.toNotificationId()
             if (!notification.actions.contains(SyncNotification.Action.Notify)) {
                 log.i { "Ignore notification $id with actions [${notification.actions.joinToString()}]" }
                 return@onEach
             }
+
+            val parsedNotification = tryOrNull {
+                Json.parseToJsonElement(notification.rawEvent).jsonObject
+            }
+            val notificationServerTs = tryOrNull {
+                parsedNotification?.get("origin_server_ts")?.jsonPrimitive?.longOrNull
+            }
+
+            if (notificationServerTs == null) {
+                log.e { "Failed to parse notification timestamp for $id" }
+                return@onEach
+            }
+            if (notificationServerTs < launchTimestamp) {
+                log.w { "Skipping past notification by timestamp for $id" }
+                return@onEach
+            }
+            val formatted = NotificationEventTextFormat.notificationToText(notification)
             log.d { "Notifying for $id" }
             Notifier.notify(
                 id = id,
