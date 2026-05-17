3d56124761378303124d32c01ce703c96699e2f7
Alessandro Grassi <alessandro@aggro.it>
2026-05-17 11:39:38 +0200

    Rename flatpak to chat.schildi.SchildiChatRevenge

--- package_flatpak.sh
+++ package_flatpak.sh
@@ -15,7 +15,7 @@ for i in 16 32 64 128 256 ; do
   magick graphics/ic_launcher.ico -resize ${i}x${i} graphics/ic_launcher_${i}x${i}.png
 done
 echo "[+] Building and installing flatpak"
-flatpak-builder flatpak --install --user org.SchildiChat.Revenge.yml --force-clean
+flatpak-builder flatpak --install --user chat.schildi.SchildiChatRevenge.yml --force-clean
 echo "[+] Done! Run from your desktop environment, or type:"
-echo "   flatpak run org.SchildiChat.Revenge"
+echo "   flatpak run chat.schildi.SchildiChatRevenge"
 
