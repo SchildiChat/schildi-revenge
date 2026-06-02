f5833e30c92ec3f7ef1cde51093da3cbb9cde1fd
SpiritCroc <dev@spiritcroc.de>
2026-06-02 21:11:09 +0200

    Add missed vertical padding in about screen
    Change-Id: Ieb0ce5763c0655318ca9c41be0a51c4327220c1f

--- composeApp/src/jvmMain/kotlin/chat/schildi/revenge/compose/destination/AboutScreen.kt
+++ composeApp/src/jvmMain/kotlin/chat/schildi/revenge/compose/destination/AboutScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -130,6 +131,7 @@ fun AboutScreen(
                         verticalArrangement = Dimens.verticalArrangement,
                         horizontalAlignment = Alignment.CenterHorizontally,
                         state = listState,
+                        contentPadding = PaddingValues(vertical = Dimens.windowPadding),
                     ) {
                         if (!isSearching) {
                             item(key = "header") {
