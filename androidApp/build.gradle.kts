0978a66fffdb9254eeba9f4aad3a8c254d46b97d
SpiritCroc <dev@spiritcroc.de>
2026-08-15 21:35:11 +0200

    [Android] minify resources
    Change-Id: I8caad0d005cd2c88cb4cf3f82a439d7026685f1e

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -71,6 +71,7 @@ android {
         }
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
